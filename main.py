6d574c59184ce95e19e3336921a5abe305cf1e28
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-13 15:01:12 -0700

    added js

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template
-from settings import NAME
+# from settings import NAME
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 #     return f"Hello, {NAME}"
-    return render_template("index.html", name=NAME)
+    return render_template("index.html")
 
 @app.route('/sales')
 def second_route():
     return render_template("sales.html")
 
-# @app.route('/test')
-# def test_route():
-#     return render_template("test1.html", message="Hello Flask!", contacts = ['c1', 'c2', 'c3', 'c4', 'c5'])
