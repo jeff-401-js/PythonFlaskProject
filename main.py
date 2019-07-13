6d48420ffafbf599980bcb74f9f8bb7d5eb25251
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-13 15:41:37 -0700

    mongo work

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template
 from flask import request
 from pymongo import MongoClient
+import json
 
 client = MongoClient(f"{MONGO}")
+db = client.tempDB
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
 #     return f"Hello, {NAME}"
     return render_template("index.html")
 
-@app.route('/sales')
+@app.route("/sales")
 def second_route():
     return render_template("sales.html")
 
+@app.route("/post", methods = ['POST'])
+def add_store_sales():
+    data = json.loads(request.data)
+    name = data["name"]
+    minimum = data["minimum"]
+    maximum = data["maximum"]
+    average = data["average"]
